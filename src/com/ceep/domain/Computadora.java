e1cf873b1dac590100329c41dabfd94d47abbb0c
Pg7Pedro <Pg91999@hotmail.com>
2021-10-06 10:43:23 +0200

    Update Computadora.java

--- src/com/ceep/domain/Computadora.java
+++ src/com/ceep/domain/Computadora.java
@@ -11,24 +11,68 @@ package com.ceep.domain;
  */
 public class Computadora {
 
-    private final int idComputadora;
-    String nombre;
+    public Computadora() {
+    
+    }
+    
+    
+    private int idComputadora;
+    private String nombre;
     Monitor monitor[];
     Teclado teclado[];
-    Raton raton[];
+    Raton Raton[];
 
-    public Computadora(String Nombre, Monitor, Teclado, Raton){
+    public Computadora(Monitor monitor,Teclado teclado,Raton Raton){
 
-        this.nombre = Nombre;
         this.monitor = monitor;
         this.teclado = teclado;
-        this.raton = raton;
+        this.Raton = Raton;
 
+    }
+
+    public int getIdComputadora() {
+        return idComputadora;
+    }
+
+    public void setIdComputadora(int idComputadora) {
+        this.idComputadora = idComputadora;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Monitor[] getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor[] monitor) {
+        this.monitor = monitor;
+    }
+
+    public Teclado[] getTeclado() {
+        return teclado;
+    }
+
+    public void setTeclado(Teclado[] teclado) {
+        this.teclado = teclado;
+    }
+
+    public Raton[] getRaton() {
+        return Raton;
+    }
+
+    public void setRaton(Raton[] Raton) {
+        this.Raton = Raton;
     }
 
     @Override
     public String toString() {
-        return "Computadora{" + "idComputadora=" + idComputadora + ", nombre=" + nombre + ", monitor=" + monitor + ", teclado=" + teclado + ", raton=" + raton + '}';
+        return "Computadora{" + "idComputadora=" + idComputadora + ", monitor=" + monitor + ", teclado=" + teclado + ", Raton=" + Raton + '}';
     }
 
 }
